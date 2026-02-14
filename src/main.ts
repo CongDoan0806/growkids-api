a2258e063e8dc0e91d57956d1a7e71ee96c85c62
Tran Cong Doan <congdoan0806@gmail.com>
2026-02-15 00:06:43 +0700

    feat(swagger): add swagger api document

--- src/main.ts
+++ src/main.ts
@@ -6,19 +6,39 @@ import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: WinstonModule.createLogger({
       instance: winstonLogger,
     }),
   });
+
   app.enableCors();
+
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new LoggingInterceptor());
   app.useGlobalInterceptors(new ResponseInterceptor());
+
   app.setGlobalPrefix('api/v1');
+
+  const config = new DocumentBuilder()
+    .setTitle('GrowKids API')
+    .setDescription('API documentation for GrowKids system')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document);
+
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
+
   console.log(`Application is running on: http://localhost:${port}/api/v1`);
+  console.log(`Swagger docs available at: http://localhost:${port}/docs`);
 }
+
 bootstrap();
